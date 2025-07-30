4b4bb60adbb2af3333295255d80903944cf52019
joyceannbariquit-dot <joyceannbariquit@gmail.com>
2025-07-30 14:24:42 +0800

    added act2

---
+++ src/joyceact2/joyceact2.java
@@ -0,0 +1,29 @@
+
+package joyceact2;
+
+import java.util.Scanner;
+
+public class joyceact2 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+        System.out.print("Enter employee name:");
+        String ename = sc.nextLine();
+        System.out.print("Enter hourly rate:");
+        float hrate = sc.nextFloat();
+        System.out.print("Enter total hours worked:");
+        float hours = sc.nextFloat();
+        
+        System.out.println("------Wage Summary-------");
+        System.out.println("Employee:"+ename);
+        System.out.println("Hourly Rate:"+hrate);
+        System.out.println("Hours Worked:"+hours);
+        System.out.println("Gross Weekly Wage:"+(hours*hrate));
+        System.out.printf("SSS Contributions(10%%) : P%.2f\n", ((hours * hrate) * 0.1));
+        System.out.println("-----------------------------------");
+        System.out.printf("Net Weekly: P%.2f\n\n",((hours*hrate) - ((hours*hrate)*0.1)));
+    
+    }
+   
+    }
+ 
+
