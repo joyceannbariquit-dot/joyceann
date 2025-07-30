3bf05ae2eb5d1d7e5dbdb8e25d0fa20a5f7d16b4
cherisho225 <catipaycherish61@gmail.com>
2025-07-29 23:37:53 -0700

    act3

---
+++ src/joyceact2/act3.java
@@ -0,0 +1,9 @@
+    package joyceact2;
+
+    import java.util.Scanner;
+    
+    public class act3 {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+}
